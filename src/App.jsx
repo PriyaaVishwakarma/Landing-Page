96782fd2c5553f5a934a66b2380c0d7d56b1983c
PriyaaVishwakarma <priyavishwakarma11107@gmail.com>
2026-01-14 11:14:51 +0530

    project 1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import HeroSection from "./Hero";
+import First from "./First";
+
+
+const App = () => {
+  return (
+    <>
+    <First/>
+    <HeroSection/>
+    
+    </>
+  );
+};
+
+export default App;
